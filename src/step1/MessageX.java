e3db8182587f90491e3460bb5cffcc2beac2f6f7
Benjam73 <besnierbenjamin73@gmail.com>
2017-12-01 09:35:05 +0100

    Init options.xml

--- src/step1/MessageX.java
+++ src/step1/MessageX.java
@@ -4,4 +4,14 @@ import jus.poc.prodcons.Message;
 
 public class MessageX implements Message {
 
+	public String toString() {
+		try {
+			throw new Exception("Not implemented yet");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+
+	}
 }
--- src/step1/TestProdCons.java
+++ src/step1/TestProdCons.java
@@ -1,9 +1,25 @@
 package step1;
 
+import java.io.IOException;
+import java.util.InvalidPropertiesFormatException;
+import java.util.Map;
+import java.util.Properties;
+
 import jus.poc.prodcons.Observateur;
 import jus.poc.prodcons.Simulateur;
 
 public class TestProdCons extends Simulateur {
+
+	int nbProd;
+	int nbCons;
+	int nbBuffer;
+	int tempsMoyenProduction;
+	int deviationTempsMoyenProduction;
+	int tempsMoyenConsommation;
+	int nombreMoyenDeProduction;
+	int deviationNombreMoyenDeProduction;
+	int nombreMoyenNbExemplaire;
+	int deviationNombreMoyenNbExemplaire;
 
 	public TestProdCons(Observateur observateur) {
 		super(observateur);
@@ -16,4 +32,24 @@ public class TestProdCons extends Simulateur {
 
 	}
 
+	protected void init(String file) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException,
+			SecurityException, InvalidPropertiesFormatException, IOException {
+		Properties properties = new Properties();
+		properties.loadFromXML(ClassLoader.getSystemResourceAsStream(file));
+		String key;
+		int value;
+		Class<?> thisOne = getClass();
+		for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+			key = (String) entry.getKey();
+			value = Integer.parseInt((String) entry.getValue());
+			thisOne.getDeclaredField(key).set(this, value);
+		}
+	}
+
+	// Fichier xml :
+
+	public static void main(String[] args) {
+		new TestProdCons(new Observateur()).start();
+		this.init(("options/" + "options.xml").toString());
+	}
 }
