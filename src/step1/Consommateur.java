d6329c08c375ab7e50bc094d0bc68432f98197cc
Benjam73 <besnierbenjamin73@gmail.com>
2017-12-06 18:49:58 +0100

    do autogenerated method

--- src/step1/Consommateur.java
+++ src/step1/Consommateur.java
@@ -7,40 +7,43 @@ import jus.poc.prodcons._Consommateur;
 
 public class Consommateur extends Acteur implements _Consommateur {
 
+	private int nbMessagesConsommes;
+
 	protected Consommateur(int type, Observateur observateur, int moyenneTempsDeTraitement,
 			int deviationTempsDeTraitement) throws ControlException {
 		super(type, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
 		// TODO Auto-generated constructor stub
+		nbMessagesConsommes = 0;
 	}
 
 	@Override
 	public int deviationTempsDeTraitement() {
-		// TODO Auto-generated method stub
-		return 0;
+		return deviationTempsDeTraitement;
 	}
 
 	@Override
 	public int identification() {
-		// TODO Auto-generated method stub
-		return 0;
+		return super.identification();
 	}
 
 	@Override
 	public int moyenneTempsDeTraitement() {
-		// TODO Auto-generated method stub
-		return 0;
+		return moyenneTempsDeTraitement;
 	}
 
 	@Override
 	public int nombreDeMessages() {
-		// TODO Auto-generated method stub
-		return 0;
+		return nbMessagesConsommes;
 	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 
+	}
+
+	public String toString() {
+		return "Consommateur" + this.identification();
 	}
 
 }
