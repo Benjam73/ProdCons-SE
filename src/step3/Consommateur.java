15cfae5a4e3c3447d4422eb70d2c60c5c1e8960f
Benjam73 <besnierbenjamin73@gmail.com>
2017-12-13 16:29:48 +0100

    step3 Over

---
+++ src/step3/Consommateur.java
@@ -0,0 +1,66 @@
+package step3;
+
+import jus.poc.prodcons.Acteur;
+import jus.poc.prodcons.Aleatoire;
+import jus.poc.prodcons.ControlException;
+import jus.poc.prodcons.Message;
+import jus.poc.prodcons.Observateur;
+import jus.poc.prodcons._Consommateur;
+
+public class Consommateur extends Acteur implements _Consommateur {
+
+	private ProdCons buffer;
+	private Aleatoire consumptionDurationRandomVariable;
+	private Integer alreadyConsume;
+	private TestProdCons simulator;
+
+	protected Consommateur(TestProdCons simulator, Observateur observateur, int moyenneTempsDeTraitement,
+			int deviationTempsDeTraitement, ProdCons buffer) throws ControlException {
+
+		super(typeConsommateur, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
+		this.buffer = buffer;
+		consumptionDurationRandomVariable = new Aleatoire(moyenneTempsDeTraitement, deviationTempsDeTraitement);
+		alreadyConsume = 0;
+		this.simulator = simulator;
+	}
+
+	@Override
+	public void run() {
+		while ((this.getBuffer().enAttente() != 0) || (simulator.hasProducer())) {
+			int timeToConsume = randomConsumptionDuration();
+			try {
+				Message removedMessage = this.getBuffer().get(this);
+				System.out.println(this.toString() + " received message " + removedMessage.toString());
+				observateur.retraitMessage(this, removedMessage);
+				sleep(timeToConsume);
+				observateur.consommationMessage(this, removedMessage, timeToConsume);
+				newMessageConsume();
+			} catch (Exception e) {
+				e.getMessage();
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private int randomConsumptionDuration() {
+		return consumptionDurationRandomVariable.next();
+	}
+
+	private void newMessageConsume() {
+		alreadyConsume++;
+	}
+
+	public String toString() {
+		return "[Consommateur " + this.identification() + "]";
+	}
+
+	@Override
+	public int nombreDeMessages() {
+		return alreadyConsume;
+	}
+
+	private ProdCons getBuffer() {
+		return this.buffer;
+	}
+
+}
